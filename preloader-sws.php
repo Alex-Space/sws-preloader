8a64de76316fc50ef5ae0ac5ff9e02100d0cc4dc
AlexSpace <cometospace@google.com>
2016-04-03 12:52:15 +0600

    Added new release 1.2 to main file

--- preloader-sws.php
+++ preloader-sws.php
@@ -2,7 +2,7 @@
 /**
 * Plugin Name: Preloader SWS
 * Description: Add preloader to your website. This plugin gives to select a lot of cool preloaders (100+). Most of them are extremely awesome! Some of them even support background changing. Also you can upload your own preloader from computer and choose a background color.
-* Version: 1.1
+* Version: 1.2
 * Author: Alex Space
 * Author URI: spwanderer@mail.ru
 */
